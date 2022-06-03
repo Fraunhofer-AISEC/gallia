935b9cacf96ec70e97e784d27e3099e60a0ed951
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2022-06-03 14:01:15 +0200

    feat: Merge find_can_ids and find_iso_tp_addr
    It works now for all cases we know.

--- src/gallia/udscan/scanner/find_can_ids.py
+++ src/gallia/udscan/scanner/find_can_ids.py
@@ -1,10 +1,12 @@
 import asyncio
 from argparse import Namespace
+from binascii import unhexlify
+from typing import Optional
 
 from gallia.transports.base import TargetURI
 from gallia.transports.can import ISOTPTransport, RawCANTransport
-from gallia.uds.core.service import NegativeResponse
-from gallia.uds.ecu import ECU
+from gallia.uds.core.service import UDSRequest, NegativeResponse
+from gallia.uds.core.client import UDSClient
 from gallia.udscan.core import DiscoveryScanner
 from gallia.utils import auto_int, can_id_repr, g_repr, write_target_list
 
@@ -21,15 +23,49 @@ class FindCanIDsScanner(DiscoveryScanner):
             "--start",
             metavar="INT",
             type=auto_int,
-            default=0x00,
+            required=True,
             help="set start address",
         )
         self.parser.add_argument(
-            "--end",
+            "--stop",
             metavar="INT",
             type=auto_int,
-            default=0x7FF,
+            required=True,
             help="set end address",
+        )
+        self.parser.add_argument(
+            "--padding",
+            type=auto_int,
+            default=None,
+            help="set isotp padding",
+        )
+        self.parser.add_argument(
+            "--pdu",
+            type=unhexlify,
+            default=bytes([0x10, 0x01]),
+            help="set pdu used for discovery",
+        )
+        self.parser.add_argument(
+            "--sleep",
+            type=float,
+            default=0.01,
+            help="set sleeptime between loop iterations",
+        )
+        self.parser.add_argument(
+            "--extended-addr",
+            action="store_true",
+            help="use extended isotp addresses",
+        )
+        self.parser.add_argument(
+            "--tester-addr",
+            type=auto_int,
+            default=0x6F1,
+            help="tester address for --extended",
+        )
+        self.parser.add_argument(
+            "--query",
+            action="store_true",
+            help="query ECU description via RDBID",
         )
         self.parser.add_argument(
             "--info-did",
@@ -40,7 +76,7 @@ class FindCanIDsScanner(DiscoveryScanner):
         )
         self.parser.add_argument(
             "--sniff-time",
-            default=60,
+            default=5,
             type=int,
             metavar="SECONDS",
             help="Time in seconds to sniff on bus for current traffic",
@@ -51,34 +87,94 @@ class FindCanIDsScanner(DiscoveryScanner):
             self.parser.error(
                 f"Unsupported transport schema {args.target.scheme}; must be can-raw!"
             )
+        if args.extended_addr and (args.start > 0xff or args.stop > 0xff):
+            self.parser.error("--start/--stop maximum value is 0xFF")
         await super().setup(args)
 
+    async def query_description(self, target_list: list[TargetURI], did: int) -> None:
+        self.logger.log_info("reading info DID from all discovered endpoints")
+        for target in target_list:
+            self.logger.log_summary("----------------------------")
+            self.logger.log_summary(f"Probing ECU: {target}")
+
+            transport = ISOTPTransport(target)
+            await transport.connect(None)
+            uds_client = UDSClient(transport, timeout=2)
+            self.logger.log_summary(f"reading device description at {g_repr(did)}")
+            try:
+                resp = await uds_client.read_data_by_identifier(did)
+                if isinstance(resp, NegativeResponse):
+                    self.logger.log_summary(f"could not read did: {resp}")
+                else:
+                    self.logger.log_summary(f"response was: {resp}")
+            except Exception as e:
+                self.logger.log_summary(f"reading description failed: {g_repr(e)}")
+
+    def _build_isotp_frame_extended(
+        self,
+        pdu: bytes,
+        ext_addr: int,
+    ) -> bytes:
+        isotp_hdr = bytes([ext_addr, len(pdu) & 0x0F])
+        return isotp_hdr + pdu
+
+    def _build_isotp_frame(self, pdu: bytes) -> bytes:
+        isotp_hdr = bytes([len(pdu) & 0x0F])
+        return isotp_hdr + pdu
+
+    def build_isotp_frame(
+        self,
+        req: UDSRequest,
+        ext_addr: Optional[int] = None,
+        padding: Optional[int] = None,
+    ) -> bytes:
+        pdu = req.pdu
+        max_pdu_len = 7 if ext_addr is None else 6
+        if len(pdu) > max_pdu_len:
+            raise ValueError("UDSRequest too large, ConsecutiveFrames not implemented")
+
+        if ext_addr is None:
+            frame = self._build_isotp_frame(pdu)
+        else:
+            frame = self._build_isotp_frame_extended(pdu, ext_addr)
+
+        if padding is not None:
+            pad_len = 8 - len(frame)
+            frame += bytes([padding]) * pad_len
+
+        return frame
+
     async def main(self, args: Namespace) -> None:
-        assert isinstance(self.transport, RawCANTransport)
+        transport = RawCANTransport(args.target)
+        await transport.connect(None)
         found = []
 
         sniff_time: int = args.sniff_time
-        self.logger.log_summary(
-            f"Listening to idle bus communication for {sniff_time}s..."
-        )
-        addr_idle = await self.transport.get_idle_traffic(sniff_time)
-        self.logger.log_summary(f"Found {len(addr_idle)} CAN Addresses on idle Bus")
-        self.transport.set_filter(addr_idle, inv_filter=True)
+        self.logger.log_summary(f"Recording idle bus communication for {sniff_time}s")
+        addr_idle = await transport.get_idle_traffic(sniff_time)
 
-        for ID in range(args.start, args.end + 1):
-            self.logger.log_info(f"Testing CAN ID {can_id_repr(ID)}")
+        self.logger.log_summary(f"Found {len(addr_idle)} CAN Addresses on idle Bus")
+        transport.set_filter(addr_idle, inv_filter=True)
+
+        req = UDSRequest.parse_dynamic(args.pdu)
+        pdu = self.build_isotp_frame(req, padding=args.padding)
+
+        for ID in range(args.start, args.stop + 1):
+            await asyncio.sleep(args.sleep)
+
+            dst_addr = args.tester_addr if args.extended_addr else ID
+            if args.extended_addr:
+                pdu = self.build_isotp_frame(req, ID, padding=args.padding)
+
+            self.logger.log_info(f"Testing ID {can_id_repr(ID)}")
             is_broadcast = False
 
-            await self.transport.sendto(
-                bytes([0x02, 0x10, 0x01, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA]),
-                timeout=0.1,
-                dst=ID,
-            )
+            await transport.sendto(pdu, timeout=0.1, dst=dst_addr)
             try:
-                addr, _ = await self.transport.recvfrom(timeout=0.1)
+                addr, _ = await transport.recvfrom(timeout=0.1)
                 if addr == ID:
                     self.logger.log_info(
-                        f"wtf!? The same CAN ID {can_id_repr(ID)} answered. Skipping..."
+                        f"The same CAN ID {can_id_repr(ID)} answered. Skipping…"
                     )
                     continue
             except asyncio.TimeoutError:
@@ -88,7 +184,7 @@ class FindCanIDsScanner(DiscoveryScanner):
                 # The recv buffer needs to be flushed to avoid
                 # wrong results...
                 try:
-                    new_addr, _ = await self.transport.recvfrom(timeout=0.1)
+                    new_addr, _ = await transport.recvfrom(timeout=0.1)
                     if new_addr != addr:
                         is_broadcast = True
                         self.logger.log_summary(
@@ -109,42 +205,37 @@ class FindCanIDsScanner(DiscoveryScanner):
                         self.logger.log_summary(
                             f"found endpoint on CAN ID [src:dst]: {can_id_repr(ID)}:{can_id_repr(addr)}"
                         )
-                        found.append(
-                            TargetURI.from_parts(
-                                ISOTPTransport.SCHEME,
-                                args.target.host,
-                                None,
-                                {
-                                    "src_addr": hex(ID),
-                                    "dst_addr": hex(addr),
-                                    "tx_padding": "0xaa",
-                                    "rx_padding": "0xaa",
-                                    "is_fd": str(self.transport.args["is_fd"]).lower(),
-                                },
-                            )
+                        target_args = {}
+                        target_args["is_fd"] = str(transport.args["is_fd"]).lower()
+                        target_args["is_extended"] = str(transport.args["is_extended"]).lower()
+
+                        if args.extended_addr:
+                            target_args["ext_address"] = hex(ID)
+                            target_args["rx_ext_address"] = args.tester_addr & 0xff
+                            target_args["src_addr"] = args.tester_addr
+                            target_args["dst_addr"] = hex(addr)
+                        else:
+                            target_args["src_addr"] = hex(ID)
+                            target_args["dst_addr"] = hex(addr)
+
+                        if args.padding is not None:
+                            target_args["tx_padding"] = f"{args.padding}"
+                        if args.padding is not None:
+                            target_args["rx_padding"] = f"{args.padding}"
+
+                        target = TargetURI.from_parts(
+                            ISOTPTransport.SCHEME,
+                            args.target.hostname,
+                            None,
+                            target_args,
                         )
+                        found.append(target)
                     break
 
         self.logger.log_summary(f"finished; found {len(found)} UDS endpoints")
         ecus_file = self.artifacts_dir.joinpath("ECUs.txt")
         self.logger.log_summary(f"Writing urls to file: {ecus_file}")
-        connection_strings = await write_target_list(ecus_file, found, self.db_handler)
+        await write_target_list(ecus_file, found, self.db_handler)
 
-        self.logger.log_info("reading info DID from all discovered endpoints")
-        did = args.info_did
-        for url in connection_strings:
-            self.logger.log_summary("----------------------------")
-            self.logger.log_summary(f"Probing ECU: {url}")
-            target = TargetURI(url)
-            transport = ISOTPTransport(target)
-            await transport.connect(None)
-            ecu = ECU(transport, timeout=2)
-            self.logger.log_summary(f"reading device description at {g_repr(did)}")
-            try:
-                resp = await ecu.read_data_by_identifier(did)
-                if isinstance(resp, NegativeResponse):
-                    self.logger.log_summary(f"could not read did: {resp}")
-                else:
-                    self.logger.log_summary(f"response was: {resp.data_record!r}")
-            except Exception as e:
-                self.logger.log_summary(f"reading description failed: {g_repr(e)}")
+        if args.query:
+            await self.query_description(found, args.info_did)
