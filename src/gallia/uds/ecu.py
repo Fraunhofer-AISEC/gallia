793271bc923527da412143e8bb8846f9cbfffe16
fabian <fabian.kuegler@aisec.fraunhofer.de>
2022-06-28 17:26:19 +0200

    refactor(vECU): improve output of response times and state information

--- src/gallia/uds/ecu.py
+++ src/gallia/uds/ecu.py
@@ -35,7 +35,7 @@ class ECUState:
         self.security_access_level = None
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({", ".join(f"{key}={repr(value)}" for key, value in self.__dict__.items())})'
+        return f'{type(self).__name__}({", ".join(f"{key}={g_repr(value)}" for key, value in self.__dict__.items())})'
 
 
 class ECU(UDSClient):
