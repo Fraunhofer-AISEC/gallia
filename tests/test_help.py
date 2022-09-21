c0ca3a5b60431336214220837827a7f9afafbc08
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2022-09-21 10:48:18 +0200

    tests: Simplify test

--- tests/test_help.py
+++ tests/test_help.py
@@ -3,18 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import subprocess
-from importlib.metadata import entry_points
-
-
-def check(command: list[str]) -> None:
-    print(f"Checking \"{' '.join(command)}\"")
-    subprocess.run(command, stdout=subprocess.DEVNULL, check=True)
 
 
 def test_help() -> None:
-    check(["gallia", "-h"])
-
-    all_entries = entry_points()
-    for entry in all_entries.select(group="console_scripts"):
-        if (e := entry.name).startswith("gallia_"):
-            check([e, "-h"])
+    subprocess.run(["gallia", "-h"], stdout=subprocess.DEVNULL, check=True)
