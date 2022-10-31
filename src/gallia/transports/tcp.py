1c8f39c23f31bc4321ee0f86ac8697746a34fdbc
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2022-10-31 10:17:04 +0100

    fix: Use correct keyword argument

--- src/gallia/transports/tcp.py
+++ src/gallia/transports/tcp.py
@@ -45,7 +45,7 @@ class TCPTransport(BaseTransport, scheme="tcp"):
         tags: list[str] | None = None,
     ) -> int:
         t = tags + ["write"] if tags is not None else ["write"]
-        self.logger.trace(data.hex(), tags=t)
+        self.logger.trace(data.hex(), extra={"tags": t})
 
         self.writer.write(data)
         await asyncio.wait_for(self.writer.drain(), timeout)
