f3bc210d0896ff9b24f86c13ec62b9bda0a955d1
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2022-11-04 09:56:34 +0100

    chore: Remove unused dep in flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
+  outputs = { self, nixpkgs }:
     with import nixpkgs { system = "x86_64-linux"; };
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
