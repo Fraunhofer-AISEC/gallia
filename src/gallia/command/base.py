c3f5ab47fdb46414b11007a62ec748652c8fb5bb
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2022-11-21 16:58:34 +0100

    fix: Do not fail when different owner of lockfile
    When the owner of the lockfile is different from
    the current user, `.touch()` fails. The lock can
    be created anyway. Only execute `.touch()` when
    the file does not exist.

--- src/gallia/command/base.py
+++ src/gallia/command/base.py
@@ -317,7 +317,8 @@ class BaseCommand(ABC):
         raise ValueError("base_dir or force_path must be different from None")
 
     def _aquire_flock(self, path: Path) -> None:
-        path.touch()
+        if not path.exists():
+            path.touch()
         self._lock_file_fd = os.open(path, os.O_RDONLY)
         try:
             # First do a non blocking flock. If waiting is required,
