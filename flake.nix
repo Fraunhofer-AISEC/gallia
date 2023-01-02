0e7d661265c847261d776133e0b29db7bf1aa95e
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2023-01-02 17:28:29 +0100

    chore: Update to poetry 1.3

--- flake.nix
+++ flake.nix
@@ -13,9 +13,9 @@
     in {
       devShell.x86_64-linux = pkgs.mkShell {
         buildInputs = with pkgs; [ 
+          poetry
           python310
-          python310Packages.poetry
-          # taplo-lsp
+          python311
         ];
         shellHook = ''
           LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [stdenv.cc.cc]}
