f13c73057e7af8c34333571d4faa50d86db1a4fa
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2024-01-04 11:26:34 +0100

    chore: Vendor pydantic-argparse for easier experiments
    https://raw.githubusercontent.com/cody-mar10/pydantic-argparse

---
+++ vendor/pydantic-argparse/pydantic_argparse/argparse/__init__.py
@@ -0,0 +1,18 @@
+"""Declarative and Typed Argument Parsing.
+
+This package contains the classes and methods required for declarative and
+typed argument parsing.
+
+The public interface exposed by this package is the `ArgumentParser` class,
+which is intended to be a *near* drop-in replacement for the Python standard
+library `argparse.ArgumentParser` - while providing declarative and typed
+argument parsing.
+"""
+
+# Local
+from pydantic_argparse.argparse.parser import ArgumentParser
+
+# Public Re-Exports
+__all__ = (
+    "ArgumentParser",
+)
