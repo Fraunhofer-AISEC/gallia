893c866b8516fc89008447ea2aca05c3ed8415e5
fabian <fabian.kuegler@aisec.fraunhofer.de>
2024-03-07 21:26:11 +0100

    added const

--- vendor/pydantic-argparse/pydantic_argparse/utils/field.py
+++ vendor/pydantic-argparse/pydantic_argparse/utils/field.py
@@ -11,6 +11,7 @@ class ArgFieldInfo(FieldInfo):
                  short: str | None,
                  metavar: str | None,
                  group: str | None,
+                 const: Any,
                  **kwargs: Unpack[_FromFieldInfoInputs]
                  ):
         super().__init__(default=default, **kwargs)
@@ -19,13 +20,15 @@ class ArgFieldInfo(FieldInfo):
         self.short = short
         self.metavar = metavar
         self.group = group
+        self.const = const
 
 
 def Field(default: Any = PydanticUndefined,
-                 positional: bool = False,
-                 short: str | None = None,
-                 metavar: str | None = None,
-                 group: str | None = None,
-                 **kwargs: Unpack[_FromFieldInfoInputs]
-                 ) -> Any:
-    return ArgFieldInfo(default, positional, short, metavar, group, **kwargs)
+          positional: bool = False,
+          short: str | None = None,
+          metavar: str | None = None,
+          group: str | None = None,
+          const: Any = PydanticUndefined,
+          **kwargs: Unpack[_FromFieldInfoInputs]
+          ) -> Any:
+    return ArgFieldInfo(default, positional, short, metavar, group, const, **kwargs)
