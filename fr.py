9b7071adb245975ee0dc569eb47b91c4f1edde3a
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2024-07-08 14:45:17 +0200

    wip

---
+++ fr.py
@@ -0,0 +1,17 @@
+import asyncio
+import sys
+
+assert sys.platform == "win32"
+
+from gallia.transports import RawFlexrayTransport, TargetURI
+
+
+async def main() -> None:
+    url = TargetURI("flexray://?slot_id=0x42")
+    tp = await RawFlexrayTransport.connect(url, None)
+    data = await tp.read()
+    print(data)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
