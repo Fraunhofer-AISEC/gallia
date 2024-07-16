e97d249bca1a549a1faf6c8a46e10043140f6b8a
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2024-07-16 15:35:50 +0200

    wip

--- src/gallia/transports/vector.py
+++ src/gallia/transports/vector.py
@@ -567,7 +567,7 @@ class FlexRayTPLegacyTransport(BaseTransport, scheme="flexray-tp-legacy"):
             if frame.counter != (counter & 0x0F):
                 raise RuntimeError(f"got unexpected consecutive counter: {frame.counter}")
 
-            read_bytes += len(frame.data) - 1
+            read_bytes += len(frame.data)
             data += frame.data
             counter = (counter + 1) & 0x0F
         return data
