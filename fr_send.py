d8ca1002b9eb0175a02d1fa7fca46954401dbb98
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2024-07-16 11:29:41 +0200

    wip

--- fr_send.py
+++ fr_send.py
@@ -3,10 +3,13 @@ import sys
 
 assert sys.platform == "win32"
 
+from gallia.log import Loglevel, setup_logging
 from gallia.transports import FlexRayTPLegacyTransport, TargetURI
 
 
 async def main() -> None:
+    setup_logging(level=Loglevel.DEBUG)
+
     url = TargetURI("flexray-tp-legacy://?dst_slot_id=59&src_slot_id=33&dst_address=0x60&src_address=0x44")
     tp = await FlexRayTPLegacyTransport.connect(url, None)
 
