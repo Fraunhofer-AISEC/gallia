51a21d33f4c9f3a3da315cd1cb2653802567edf4
fabian <fabian.kuegler@aisec.fraunhofer.de>
2024-07-26 18:16:30 +0200

    Plugins rework

--- src/gallia/cli.py
+++ src/gallia/cli.py
@@ -7,80 +7,50 @@ import sys
 from typing import Any
 
 import argcomplete
-from pydantic import Field
+from pydantic import Field, create_model
 from pydantic_argparse import ArgumentParser, BaseCommand
 
-from gallia.commands.primitive.uds.dtc import (
-    ClearDTCPrimitiveConfig,
-    ControlDTCPrimitiveConfig,
-    DTCPrimitive,
-    ReadDTCPrimitiveConfig,
-)
-from gallia.commands.primitive.uds.rdbi import (
-    ReadByIdentifierPrimitive,
-    ReadByIdentifierPrimitiveConfig,
-)
-from gallia.commands.primitive.uds.wdbi import (
-    WriteByIdentifierPrimitive,
-    WriteByIdentifierPrimitiveConfig,
-)
 from gallia.config import load_config_file
 from gallia.log import Loglevel, setup_logging
+from gallia.plugins.plugin import Command, CommandTree, load_commands
 
 setup_logging(Loglevel.DEBUG)
+
 config, _ = load_config_file()
+gallia_tree_model_counter = 0
+extra_defaults = {}
 
 
-# Mockup
-def create_parser_tree() -> tuple[type[BaseCommand], dict[type, dict[str, Any]]]:
-    setattr(ReadByIdentifierPrimitiveConfig, "_class", ReadByIdentifierPrimitive)
-    setattr(WriteByIdentifierPrimitiveConfig, "_class", WriteByIdentifierPrimitive)
-    for cls in [ReadDTCPrimitiveConfig, ClearDTCPrimitiveConfig, ControlDTCPrimitiveConfig]:
-        setattr(cls, "_class", DTCPrimitive)
+def create_parser_from_command_tree(command_tree: CommandTree) -> type[BaseCommand]:
+    global gallia_tree_model_counter
+    gallia_tree_model_counter += 1
+    args: dict[str, tuple[type, Field]] = {}
 
-    extra_defaults = {}
+    for key, value in command_tree.subtree.items():
+        if isinstance(value, Command):
+            args[key] = (value.config | None, Field(None, description=value.description))
+            config_attributes = value.config.attributes_from_config(config)
+            env_attributes = value.config.attributes_from_env()
+            config_attributes.update(env_attributes)
+            extra_defaults[value.config] = config_attributes
+            setattr(value.config, "_class", value.command)
+        else:
+            model_type = create_parser_from_command_tree(value)
+            args[key] = (model_type | None, Field(None, description=value.description))
 
-    for cls in [
-        ReadByIdentifierPrimitiveConfig,
-        WriteByIdentifierPrimitiveConfig,
-        ReadDTCPrimitiveConfig,
-        ClearDTCPrimitiveConfig,
-        ControlDTCPrimitiveConfig,
-    ]:
-        config_attributes = cls.attributes_from_config(config)
-        env_attributes = cls.attributes_from_env()
-        config_attributes.update(env_attributes)
-        extra_defaults[cls] = config_attributes
+    return create_model(
+        f"gallia_tree_model_{gallia_tree_model_counter}", __base__=BaseCommand, **args
+    )
 
-    class DTC(BaseCommand):
-        read: ReadDTCPrimitiveConfig | None = Field(None, description="Read DTCs")
-        clear: ClearDTCPrimitiveConfig | None = Field(None, description="Clear DTCs")
-        control: ControlDTCPrimitiveConfig | None = Field(None, description="Control DTCs")
 
-    class Primitive(BaseCommand):
-        rdbi: ReadByIdentifierPrimitiveConfig | None = Field(
-            None,
-            description="Read data at a specific ID using the UDS service RDBI (0x22)",
-        )
-        wdbi: WriteByIdentifierPrimitiveConfig | None = Field(
-            None,
-            description="Write data at a specific ID using the UDS service WDBI (0x2e)",
-        )
-        dtc: DTC | None = Field(None, description="Read, delete or control DTCs")
-        # iocbi: ...
-        # ...
+def create_plugin_parser() -> tuple[type[BaseCommand], dict[type, dict[str, Any]]]:
+    gallia_commands = CommandTree("gallia", load_commands())
 
-    class Gallia(BaseCommand):
-        primitive: Primitive | None = Field(None, description="Simple UDS scripts")
-        # scan
-        # discover
-        # ...
-
-    return Gallia, extra_defaults
+    return create_parser_from_command_tree(gallia_commands), extra_defaults
 
 
 def main() -> None:
-    model, extra_defaults = create_parser_tree()
+    model, extra_defaults = create_plugin_parser()
     parser = ArgumentParser(model=model, extra_defaults=extra_defaults)
     argcomplete.autocomplete(parser)
     _, config = parser.parse_typed_args()
