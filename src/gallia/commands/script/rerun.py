d5ad9509e7370b93dce08de58827555f30489f84
fabian <fabian.kuegler@aisec.fraunhofer.de>
2024-10-10 20:48:08 +0200

    Added rerun script

---
+++ src/gallia/commands/script/rerun.py
@@ -0,0 +1,59 @@
+import asyncio
+import importlib
+import json
+import sys
+from typing import Self, Any
+
+import aiosqlite
+from pydantic import model_validator
+
+from gallia.command import BaseCommand
+from gallia.command.base import ScriptConfig, Script
+from gallia.log import get_logger
+from gallia.command.config import Field
+
+logger = get_logger(__name__)
+
+
+class RerunnerConfig(ScriptConfig):
+    id: int = Field(positional=True, description="The id of the run_meta entry in the db to rerun")
+
+    @model_validator(mode="after")
+    def check_transport_requirements(self) -> Self:
+        if self.db is None:
+            raise ValueError("This script requires a database connection")
+
+        return self
+
+class Rerunner(Script):
+    CONFIG_TYPE = RerunnerConfig
+    SHORT_HELP = "Rerun a previous gallia command based on its run_meta in the database"
+
+    def __init__(self, config: RerunnerConfig):
+        super().__init__(config)
+        self.config: RerunnerConfig = config
+
+    def main(self) -> None:
+        query = ("SELECT script, config "
+                 "FROM run_meta "
+                 "WHERE id = ?")
+        parameters = (self.config.id, )
+        cursor: aiosqlite.Cursor = asyncio.run(self.db_handler.connection.execute(query, parameters))
+        row = asyncio.run(cursor.fetchone())
+
+        if row is None:
+            logger.error(f"There id no run_meta entry with the id {self.config.id}")
+            return
+
+        script: str = row[0]
+        config: dict[str, Any] = json.loads(row[1])
+        script_parts = script.split('.')
+        module = '.'.join(script_parts[:-1])
+        class_name = script_parts[-1]
+
+        logger.info(f"Rerunning run {self.config.id} ({class_name}) with: {config}")
+
+        gallia_class: type[BaseCommand] = getattr(importlib.import_module(module), class_name)
+        command = gallia_class(gallia_class.CONFIG_TYPE(**config))
+
+        sys.exit(command.entry_point())
