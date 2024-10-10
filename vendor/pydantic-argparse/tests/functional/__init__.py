df0df97ae6a2fb9871e26d8a790b536555ed2f0e
fabian <fabian.kuegler@aisec.fraunhofer.de>
2024-10-10 20:55:57 +0200

    Remove outdated test cases

--- vendor/pydantic-argparse/tests/functional/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Hayden Richards
-#
-# SPDX-License-Identifier: MIT
-
-"""Functional Tests for the `pydantic-argparse` Package.
-
-This file is required to mark the unit tests as a package, so they can resolve
-and import the actual top level package code
-"""
