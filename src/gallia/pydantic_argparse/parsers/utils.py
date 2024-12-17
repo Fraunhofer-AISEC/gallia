c2dcee3419a940b36f46dd945748c9e0a9aee80a
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2024-12-17 19:25:48 +0100

    chore: Remove TypeVar stuff and use native generic syntax

--- src/gallia/pydantic_argparse/parsers/utils.py
+++ src/gallia/pydantic_argparse/parsers/utils.py
@@ -6,23 +6,21 @@
 
 from argparse import Action, FileType
 from collections.abc import Callable, Iterable
-from typing import Any, Protocol, TypeVar
-
-_T = TypeVar("_T")
+from typing import Any, Protocol
 
 
 class SupportsAddArgument(Protocol):
     """ArgumentParser protocol that captures the base parser and argument groups."""
 
-    def add_argument(  # noqa: D102
+    def add_argument[T](  # noqa: D102
         self,
         *name_or_flags: str,
         action: str | type[Action] = ...,
         nargs: int | str = ...,
         const: Any = ...,
         default: Any = ...,
-        type: Callable[[str], _T] | FileType = ...,  # noqa: A002
-        choices: Iterable[_T] | None = ...,
+        type: Callable[[str], T] | FileType = ...,  # noqa: A002
+        choices: Iterable[T] | None = ...,
         required: bool = ...,
         help: str | None = ...,  # noqa: A002
         metavar: str | tuple[str, ...] | None = ...,
