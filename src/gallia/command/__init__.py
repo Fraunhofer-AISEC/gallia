1b81121c1c3cc749598664e8cb7756365ed73661
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2025-02-28 14:50:49 +0100

    refactor: Make entry_point() async
    This avoids starting multiple event loops within gallia. With background
    tasks this causes problems when using gallia as a library. Let's just
    create one event loop at the top layer for the lifetime of the whole
    program

--- src/gallia/command/__init__.py
+++ src/gallia/command/__init__.py
@@ -2,13 +2,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from gallia.command.base import AsyncScript, BaseCommand, Scanner, Script
+from gallia.command.base import AsyncScript, BaseCommand, Scanner
 from gallia.command.uds import UDSDiscoveryScanner, UDSScanner
 
 __all__ = [
     "BaseCommand",
     "AsyncScript",
-    "Script",
     "Scanner",
     "UDSScanner",
     "UDSDiscoveryScanner",
