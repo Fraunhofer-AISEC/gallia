c480c8ce3b39ae0a16ed639e90e2b9bb28d5ea08
Jarisch, Ferdinand <ferdinand.jarisch@aisec.fraunhofer.de>
2025-03-28 12:50:56 +0100

    feat(ping): Make --session optional and wait after ping

--- src/gallia/commands/primitive/uds/ping.py
+++ src/gallia/commands/primitive/uds/ping.py
@@ -21,7 +21,7 @@ class PingPrimitiveConfig(UDSScannerConfig):
         cli_group=UDSScannerConfig._cli_group,
         config_section=UDSScannerConfig._config_section,
     )
-    session: AutoInt = Field(0x01, description="set session to perform test")
+    session: AutoInt | None = Field(None, description="set session to perform test")
     count: AutoInt | None = Field(None, description="limit number of pings to this amount")
     interval: float = Field(0.5, description="time interval between two pings", metavar="SECONDS")
 
@@ -37,18 +37,19 @@ class PingPrimitive(UDSScanner):
         self.config: PingPrimitiveConfig = config
 
     async def main(self) -> None:
-        resp = await self.ecu.set_session(self.config.session)
-        if isinstance(resp, NegativeResponse):
-            logger.error(f"Could not change to requested session: {resp}")
-            sys.exit(1)
+        if self.config.session is not None:
+            resp = await self.ecu.set_session(self.config.session)
+            if isinstance(resp, NegativeResponse):
+                logger.error(f"Could not change to requested session: {resp}")
+                sys.exit(1)
 
         i = 1
         while True:
-            if self.config.count is not None and i > self.config.count:
-                break
             ret = await self.ecu.ping()
             if isinstance(ret, NegativeResponse):
                 logger.warning(ret)
             logger.result("ECU is alive!")
+            if self.config.count is not None and i >= self.config.count:
+                break
             await asyncio.sleep(self.config.interval)
             i += 1
