8f5704f4808a03601347696c588dbcb88e94783e
Stefan Tatschner <stefan.tatschner@aisec.fraunhofer.de>
2025-09-09 11:30:53 +0200

    tests: Refactor tests to use pytest.mark.parametrize()

--- tests/pytest/test_can_message.py
+++ tests/pytest/test_can_message.py
@@ -6,17 +6,21 @@ import sys
 
 assert sys.platform.startswith("linux"), "unsupported platform"
 
+import pytest
+
 from gallia.transports.can import CANMessage
 
 
-def test_can_message_deserialization() -> None:
-    test_table = [
+# TODO: Add more test cases
+@pytest.mark.parametrize(
+    "serialized,can_message",
+    [
         (
             b"\x00\x07\x00\x00\x04\x00\x00\x00s\x02>\x00\x00\x00\x00\x00",
             CANMessage(arbitration_id=0x700, dlc=4, data=bytes([0x73, 0x02, 0x3E, 0x00])),
-        ),
-    ]
-
-    for input_, output in test_table:
-        frame = CANMessage.unpack(input_)
-        assert frame == output
+        )
+    ],
+)
+def test_can_message_deserialization(serialized: bytes, can_message: CANMessage) -> None:
+    frame = CANMessage.unpack(serialized)
+    assert frame == can_message
