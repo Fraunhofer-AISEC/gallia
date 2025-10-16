7a77d36c8d884d442d10633a4b83d676c390097b
Jarisch, Ferdinand <ferdinand.jarisch@aisec.fraunhofer.de>
2025-10-16 09:28:46 +0200

    refactor(pdu): Make `GenericPDUPrimitive` an `AsyncScript`

--- src/gallia/commands/primitive/generic/pdu.py
+++ src/gallia/commands/primitive/generic/pdu.py
@@ -3,13 +3,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from gallia.command import Scanner
-from gallia.command.base import ScannerConfig
-from gallia.command.config import Field, HexBytes
+from typing import Any
+
+from pydantic import field_serializer
+
+from gallia.command import AsyncScript
+from gallia.command.base import AsyncScriptConfig
+from gallia.command.config import Field, HexBytes, Idempotent
 from gallia.command.uds import UDSScannerConfig
+from gallia.plugins.plugin import load_transport
+from gallia.transports.base import TargetURI
 
 
-class GenericPDUPrimitiveConfig(ScannerConfig):
+class GenericPDUPrimitiveConfig(AsyncScriptConfig):
     properties: bool = Field(
         False,
         description="Read and store the ECU proporties prior and after scan",
@@ -17,9 +23,19 @@ class GenericPDUPrimitiveConfig(ScannerConfig):
         config_section=UDSScannerConfig._config_section,
     )
     pdu: HexBytes = Field(description="raw pdu to send", positional=True)
+    target: Idempotent[TargetURI] = Field(
+        description="URI that describes the target", metavar="TARGET"
+    )
+
+    @field_serializer("target")
+    def serialize_target_uri(self, target_uri: TargetURI | None) -> Any:
+        if target_uri is None:
+            return None
+
+        return target_uri.raw
 
 
-class GenericPDUPrimitive(Scanner):
+class GenericPDUPrimitive(AsyncScript):
     """A raw scanner to send a plain pdu"""
 
     CONFIG_TYPE = GenericPDUPrimitiveConfig
@@ -30,4 +46,8 @@ class GenericPDUPrimitive(Scanner):
         self.config: GenericPDUPrimitiveConfig = config
 
     async def main(self) -> None:
-        await self.transport.write(self.config.pdu)
+        transport = load_transport(self.config.target)
+
+        await transport.connect()
+
+        await transport.write(self.config.pdu)
